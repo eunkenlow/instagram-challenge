104b867e3b9f066f5940840d261a0dcebc5e2f63
eunkenlow <eunkenlow@gmail.com>
2016-11-20 23:43:25 +0000

    added code until comments

---
+++ instagram/app/controllers/posts_controller.rb
@@ -0,0 +1,63 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :owned_post, only: [:edit, :update, :destroy] 
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      flash[:success] = "Post successfully created."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your post could not be created. Please try again."
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Post successfully updated."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption, :location)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def owned_post
+    unless current_user == @post.user
+      redirect_to root_path
+    end
+  end
+
+end
